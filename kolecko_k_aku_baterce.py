6accb527ed5f400d402e622fd531527d329e309c
pvranik <petr.vranik@mgm-tp.com>
2024-07-27 11:05:58 +0200

    kolecko k aku baterce

--- kolecko_k_aku_baterce.py
+++ kolecko_k_aku_baterce.py
@@ -10,6 +10,7 @@ wp = wp.faces("<Z").workplane(invert=True).rect(22, 40).cutBlind(4.6)
 
 wp = wp.faces(">Z").workplane(invert=True).moveTo(11, 5).rect(2, 2).cutBlind(7)
 wp = wp.faces(">Z").workplane(invert=True).moveTo(-14, 5).rect(8, 2).cutBlind(7)
+
 wp = wp.faces(">Z").workplane(invert=False).cylinder(2.2, 13.3 / 2, centered=[True, True, False])
 wp = wp.faces(">Z").workplane(invert=False).cylinder(2.2, 11.5 / 2, centered=[True, True, False])
 wp = wp.faces(">Z").workplane(invert=False).cylinder(3.6, 13.3 / 2, centered=[True, True, False])
