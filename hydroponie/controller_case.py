2e12b860d2d76a3b85058ef85a5af750250943a9
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-05-07 10:38:12 +0200

    display cut-out

--- hydroponie/controller_case.py
+++ hydroponie/controller_case.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 TTGO_WIDTH = 25.09
 TTGO_HEIGHT = 51.49
 TTGO_HEIGHT_WITH_USB = 53
@@ -17,6 +20,12 @@ RESET_CENTER_OFFSET_Y = 11
 RESET_HEIGHT = 4.5
 VOLNY_KONEC = 5.4
 
+WALL_THICKNESS = 1
+
+CASE_HEIGHT = TTGO_HEIGHT_WITH_USB + WALL_THICKNESS * 2
+CASE_WIDTH = TTGO_WIDTH + WALL_THICKNESS * 2
+CASE_DEPTH = 30
+
 import cadquery as cq
 
 from common import render
@@ -24,4 +33,20 @@ from common import render
 wp = cq.Workplane("XY")
 
 
-render(wp, 'proticakaci_tyce.stl')
+def shell():
+    global wp
+    wp = wp.box(CASE_WIDTH, CASE_HEIGHT, CASE_DEPTH, centered=[True, False, False])
+    wp = wp.faces(">Z").workplane().move(0, WALL_THICKNESS).rect(CASE_WIDTH - WALL_THICKNESS * 2,
+                                                                 CASE_HEIGHT - WALL_THICKNESS * 2,
+                                                                 centered=[True, False]).cutBlind(
+        -CASE_DEPTH + WALL_THICKNESS)
+
+
+shell()
+
+wp = (wp.faces("<Z").workplane(offset=-WALL_THICKNESS)
+      .move(0, -TTGO_HEIGHT_WITH_USB + DISPLAY_OFFSET)
+      .rect(DISPLAY_WIDTH, DISPLAY_HEIGHT, centered=[True, False])
+      .cutBlind(WALL_THICKNESS, taper=-80))
+
+render(wp, 'hydroponic_controller_ttgo_case.stl')
