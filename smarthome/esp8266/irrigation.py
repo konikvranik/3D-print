d0ec7adfe8a86f9b25fff503e6100b7688e69a03
Petr Vraník <Petr.Vranik@mgm-tp.com>
2025-07-14 12:54:00 +0200

    refactored

--- smarthome/esp8266/irrigation.py
+++ smarthome/esp8266/irrigation.py
@@ -1,37 +1,41 @@
 import os
-
 import cadquery as cq
 from cadquery import Workplane
-
 from common import render
 
+# --- Environment setup ---
 os.environ["FONTCONFIG_FILE"] = "/etc/fonts/fonts.conf"
 os.environ["FONTCONFIG_PATH"] = "/etc/fonts/"
 
-width = 90
-height = 165
-board = 1.45
-depth = 16.7 - board
-bottom = 4 - board
-wall = 1.5
+# --- Dimensions ---
+WIDTH = 90
+HEIGHT = 165
+BOARD_THICKNESS = 1.45
+DEPTH = 16.7 - BOARD_THICKNESS
+BOTTOM_HEIGHT = 4 - BOARD_THICKNESS
+WALL_THICKNESS = 1.5
 
-hole_dist = 81.5
-hole_from_border = 3.7
-hole_dia = 2.75
+# --- Mounting holes ---
+HOLE_DISTANCE = 81.5
+HOLE_BORDER_OFFSET = 3.7
+HOLE_DIAMETER = 2.75
 
-41
-131.5
+# --- Slot and cutout parameters ---
+SLOT_WIDTH = 0.5
+SLOT_SPACING = 15 + 1.5
+SLOT_START_OFFSET = 2 - 1.5 / 2
+SLOT_HEIGHT = 131.5
 
-2 - 1.5 / 2
+# --- Text and cutout offsets ---
+TEXT_OFFSET = 2
 
-text_offset = 2
 
-
-def sloupek(face, x, y, depth=depth, diameter=hole_dia) -> Workplane:
-    return (face.tag("my_face")
+def create_mounting_post(face, x, y, depth=DEPTH, diameter=HOLE_DIAMETER) -> Workplane:
+    """Create a mounting post with a hole."""
+    return (face.tag("mounting_face")
             .moveTo(x, y)
-            .rect(hole_dia * 2, hole_from_border * 2).extrude(depth)
-            .workplaneFromTagged("my_face")
+            .rect(diameter * 2, HOLE_BORDER_OFFSET * 2).extrude(depth)
+            .workplaneFromTagged("mounting_face")
             .workplane(offset=depth)
             .moveTo(x, y)
             .circle(diameter / 2)
@@ -39,64 +43,106 @@ def sloupek(face, x, y, depth=depth, diameter=hole_dia) -> Workplane:
             )
 
 
-wp = cq.Workplane("XY")
-wp = wp.box(width + wall * 2, height + wall * 2, depth + bottom + board + wall, centered=True)
-wp = wp.edges().filter(lambda e: e.Center().z < wp.faces(">Z").val().Center().z).fillet(2)
-wp = wp.faces(">Z").rect(width, height).cutBlind(-depth - bottom - board)
+def add_partitions(wp):
+    """Add vertical and horizontal ventilation slots."""
+    # Vertical slot
+    wp = wp.faces("<Z").workplane(offset=WALL_THICKNESS, invert=True)
+    wp = wp.moveTo(0, (-HEIGHT + SLOT_HEIGHT) / 2 + SLOT_START_OFFSET)
+    wp = wp.box(SLOT_WIDTH, SLOT_HEIGHT, DEPTH - 1, centered=[True, True, False])
+    # Horizontal slots
+    for i in range(9):
+        wp = wp.faces("<Z").workplane(offset=WALL_THICKNESS, invert=True)
+        y_pos = -HEIGHT / 2 + SLOT_START_OFFSET + i * SLOT_SPACING
+        wp = wp.moveTo(0, y_pos).box(WIDTH, SLOT_WIDTH, DEPTH - 1, centered=[True, True, False])
+    return wp
 
-wp = wp.faces("<Z").workplane(offset=wall, invert=True)
-wp = wp.moveTo(0, (-height + 131.5) / 2 + (2 - 1.5 / 2)).box(.5, 131.5, depth - 1, centered=[True, True, False])
-for i in range(0, 9):
-    wp = wp.faces("<Z").workplane(offset=wall, invert=True)
-    wp = wp.moveTo(0, - height / 2 + (2 - 1.5 / 2) + i * (15 + 1.5)).box(width, .5, depth - 1,
-                                                                         centered=[True, True, False])
 
-wp = wp.faces("<Z[-2]").workplane().tag("spodni_dno")
-wp = sloupek(wp, hole_dist / 2, (height / 2 - hole_from_border), diameter=hole_dia / 1.5)
-wp = sloupek(wp.workplaneFromTagged("spodni_dno"), -hole_dist / 2, (height / 2 - hole_from_border),
-             diameter=hole_dia / 1.5)
-wp = sloupek(wp.workplaneFromTagged("spodni_dno"), 0, -(height / 2 - hole_from_border), diameter=hole_dia / 1.5)
-
-for i in range(0, 8):
+def add_side_cutouts(wp):
+    """Add cutouts on both sides of the case."""
+    for i in range(8):
+        y_pos = -HEIGHT / 2 + SLOT_START_OFFSET + i * SLOT_SPACING + SLOT_SPACING / 2
+        z_pos = (-BOARD_THICKNESS - BOTTOM_HEIGHT - 2) / 2 + WALL_THICKNESS
+        wp = wp.faces(">X").workplane()
+        wp = wp.moveTo(y_pos, z_pos)
+        wp = wp.rect(12.6, 5 + BOARD_THICKNESS + BOTTOM_HEIGHT + 2, centered=True).cutBlind(-WALL_THICKNESS)
+    # Final right side cutout
     wp = wp.faces(">X").workplane()
-    wp = wp.moveTo(-height / 2 + (2 - 1.5 / 2) + i * (15 + 1.5) + (15 + 1.5) / 2, (-board - bottom - 2) / 2 + wall)
-    wp = wp.rect(12.6, 5 + board + bottom + 2, centered=True).cutBlind(-wall)
+    wp = wp.moveTo(HEIGHT / 2 - 15.5, (-BOARD_THICKNESS - BOTTOM_HEIGHT - 2) / 2 + WALL_THICKNESS)
+    wp = wp.rect(8, 5 + BOARD_THICKNESS + BOTTOM_HEIGHT + 2, centered=True).cutBlind(-WALL_THICKNESS)
+    # Left side cutouts (mirror)
+    for i in range(8):
+        y_pos = -(-HEIGHT / 2 + SLOT_START_OFFSET + i * SLOT_SPACING + SLOT_SPACING / 2)
+        z_pos = (-BOARD_THICKNESS - BOTTOM_HEIGHT - 2) / 2 + WALL_THICKNESS
+        wp = wp.faces("<X").workplane()
+        wp = wp.moveTo(y_pos, z_pos)
+        wp = wp.rect(12.6, 5 + BOARD_THICKNESS + BOTTOM_HEIGHT + 2, centered=True).cutBlind(-WALL_THICKNESS)
+    return wp
 
-wp = wp.faces(">X").workplane()
-wp = wp.moveTo(height / 2 - 15.5, (-board - bottom - 2) / 2 + wall).rect(8, 5 + board + bottom + 2,
-                                                                         centered=True).cutBlind(-wall)
 
-for i in range(0, 8):
-    wp = wp.faces("<X").workplane()
-    wp = wp.moveTo(-(-height / 2 + (2 - 1.5 / 2) + i * (15 + 1.5) + (15 + 1.5) / 2), (-board - bottom - 2) / 2 + wall)
-    wp = wp.rect(12.6, 5 + board + bottom + 2, centered=True).cutBlind(-wall)
+def add_top_cutouts(wp):
+    """Add cutouts and extrusions on the top face."""
+    # Large rectangle cutout and extrusion
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(9.5, HEIGHT / 2 - 9 / 2)
+    wp = wp.rect(19, 10, centered=True).extrude(DEPTH)
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(9.5, HEIGHT / 2 - 9 / 2)
+    wp = wp.rect(18, 9, centered=True).cutThruAll()
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(10, HEIGHT / 2 - 9 / 2)
+    wp = wp.rect(18, 8, centered=True).extrude(WALL_THICKNESS)
+    # Small rectangle cutout and extrusion
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(5, HEIGHT / 2 - 8 / 2)
+    wp = wp.rect(6, 4, centered=True).extrude(DEPTH - 1)
+    # Left side rectangle cutout and extrusion
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(-7.5, HEIGHT / 2 - 9 / 2)
+    wp = wp.rect(7, 5, centered=True).extrude(DEPTH)
+    wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(-7.5, HEIGHT / 2 - 9 / 2)
+    wp = wp.rect(6, 4, centered=True).cutBlind(DEPTH)
+    return wp
 
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(9.5, height / 2 - 9 / 2)
-wp = wp.rect(18 + 1, 9 + 1, centered=True).extrude(depth)
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(9.5, height / 2 - 9 / 2)
-wp = wp.rect(18, 9, centered=True).cutThruAll()
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(10, height / 2 - 9 / 2)
-wp = wp.rect(18, 8, centered=True).extrude(wall)
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(5, height / 2 - 8 / 2)
-wp = wp.rect(6, 4, centered=True).extrude(depth - 1)
 
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(-7.5, height / 2 - 9 / 2)
-wp = wp.rect(6 + 1, 4 + 1, centered=True).extrude(depth)
-wp = wp.faces("<Z").workplane(centerOption="CenterOfBoundBox", invert=True).moveTo(-7.5, height / 2 - 9 / 2)
-wp = wp.rect(6, 4, centered=True).cutBlind(depth)
+def add_mounting_posts(wp, tag_face, post_depth, diameter=HOLE_DIAMETER):
+    """Add three mounting posts to the given face."""
+    positions = [
+        (HOLE_DISTANCE / 2, HEIGHT / 2 - HOLE_BORDER_OFFSET),
+        (-HOLE_DISTANCE / 2, HEIGHT / 2 - HOLE_BORDER_OFFSET),
+        (0, -(HEIGHT / 2 - HOLE_BORDER_OFFSET))
+    ]
+    for x, y in positions:
+        wp = create_mounting_post(wp.workplaneFromTagged(tag_face), x, y, depth=post_depth, diameter=diameter)
+    return wp
 
-render(wp, 'irrigation.stl')
 
-wp = cq.Workplane("XY")
-wp = wp.box(width + wall * 2, height + wall * 2, wall, centered=True)
+def build_main_case():
+    """Build the main case."""
+    wp = cq.Workplane("XY")
+    wp = wp.box(WIDTH + WALL_THICKNESS * 2, HEIGHT + WALL_THICKNESS * 2, DEPTH + BOTTOM_HEIGHT + BOARD_THICKNESS + WALL_THICKNESS, centered=True)
+    wp = wp.edges().filter(lambda e: e.Center().z < wp.faces(">Z").val().Center().z).fillet(2)
+    wp = wp.faces(">Z").rect(WIDTH, HEIGHT).cutBlind(-DEPTH - BOTTOM_HEIGHT - BOARD_THICKNESS)
+    wp = add_partitions(wp)
+    wp = wp.faces("<Z[-2]").workplane().tag("bottom_face")
+    wp = add_mounting_posts(wp, "bottom_face", post_depth=DEPTH, diameter=HOLE_DIAMETER / 1.5)
+    wp = add_side_cutouts(wp)
+    wp = add_top_cutouts(wp)
+    return wp
 
-wp = wp.faces("<Z").workplane(invert=True).tag("spodni_dno")
-wp = sloupek(wp, hole_dist / 2, (height / 2 - hole_from_border), depth=bottom + wall)
-wp = sloupek(wp.workplaneFromTagged("spodni_dno"), -hole_dist / 2, (height / 2 - hole_from_border), depth=bottom + wall)
-wp = sloupek(wp.workplaneFromTagged("spodni_dno"), 0, -(height / 2 - hole_from_border), depth=bottom + wall)
 
-wp = sloupek(wp.faces("<Z").workplane(invert=True), 0, -(height / 2 + hole_from_border + wall), depth=wall)
-wp = sloupek(wp.faces("<Z").workplane(invert=True), 0, (height / 2 + hole_from_border + wall), depth=wall)
-wp = wp.edges("<Z").fillet(.68)
+def build_bottom_plate():
+    """Build the bottom plate."""
+    wp = cq.Workplane("XY")
+    wp = wp.box(WIDTH + WALL_THICKNESS * 2, HEIGHT + WALL_THICKNESS * 2, WALL_THICKNESS, centered=True)
+    wp = wp.faces("<Z").workplane(invert=True).tag("bottom_face")
+    wp = add_mounting_posts(wp, "bottom_face", post_depth=BOTTOM_HEIGHT + WALL_THICKNESS)
+    # Additional mounting posts
+    wp = create_mounting_post(wp.faces("<Z").workplane(invert=True), 0, -(HEIGHT / 2 + HOLE_BORDER_OFFSET + WALL_THICKNESS), depth=WALL_THICKNESS)
+    wp = create_mounting_post(wp.faces("<Z").workplane(invert=True), 0, (HEIGHT / 2 + HOLE_BORDER_OFFSET + WALL_THICKNESS), depth=WALL_THICKNESS)
+    wp = wp.edges("<Z").fillet(.68)
+    return wp
 
-render(wp, 'irrigation_bottom.stl')
+
+def main():
+    """Main workflow."""
+    render(build_main_case(), 'irrigation.stl')
+    render(build_bottom_plate(), 'irrigation_bottom.stl')
+
+
+if __name__ == "__main__":
+    main()
