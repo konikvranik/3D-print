19e3bd78db9d57c0444fe59d99a23aa0dfdd4b73
Petr Vraník <Petr.Vranik@mgm-tp.com>
2026-01-16 18:21:16 +0100

    add function to calculate PLA bore diameter and create reolink case model

--- common.py
+++ common.py
@@ -16,3 +16,36 @@ def render(object , fname, tolerance=0.0001, angularTolerance=0.1):
         object.export(fname, tolerance=tolerance, angularTolerance=angularTolerance)
     else:
         cq.exporters.export(object, fname, None, tolerance, angularTolerance)
+
+
+def calculate_pla_bore_diameter(screw_diameter: float) -> float:
+    """
+    Calculates the recommended pilot hole diameter for 3D printed PLA parts.
+
+    The calculation accounts for material stiffness and 3D printing
+    inner hole shrinkage. For PLA, a hole diameter of roughly 80-85%
+    of the screw diameter is ideal for standard wood/plastic screws.
+
+    Args:
+        screw_diameter (float): The nominal outer diameter of the screw in mm.
+
+    Returns:
+        float: The calculated drill/bore diameter for the 3D model.
+    """
+    if screw_diameter < 2.0:
+        # Very small screws need more clearance to prevent snapping
+        ratio = 0.85
+    elif screw_diameter <= 4.0:
+        # Standard range (3mm - 4mm)
+        ratio = 0.82
+    else:
+        # Larger screws
+        ratio = 0.80
+
+    # Standard formula: (Diameter * ratio) + 3D printing compensation
+    # Most hobbyist printers shrink internal holes by ~0.1mm to 0.2mm
+    printing_compensation = 0.1
+
+    calculated_hole = (screw_diameter * ratio) + printing_compensation
+
+    return round(calculated_hole, 2)
---
+++ smarthome/reolink_zvonek.py
@@ -0,0 +1,49 @@
+import cadquery as cq
+
+from common import calculate_pla_bore_diameter
+from common import render
+
+width = 48
+height = 133
+between_holes = 77
+screw_hole = 4
+drat_branky = 5
+napajeci_kabel = 5
+
+
+def build_case():
+    wp = cq.Workplane("XY")
+    inner_body = wp.slot2D(height, width).extrude(7)
+    inner_body = inner_body.moveTo(10.5, 0).slot2D(44, 20)
+    inner_body = inner_body.moveTo(38.5, 0).circle(screw_hole / 2).moveTo(-38.5, 0).circle(screw_hole / 2).cutThruAll()
+    inner_body = inner_body.faces('<Y').workplane().moveTo(25 + drat_branky / 2, 0).circle(drat_branky / 2).moveTo(
+        -25 - drat_branky / 2, 0).circle(
+        drat_branky / 2).cutThruAll()
+
+    hole_offset = napajeci_kabel / 2
+    outer_body = wp.slot2D(height, width).extrude(15)
+    outer_body = outer_body.faces('<Z').workplane().moveTo(10.5, 0).slot2D(44, 20).cutBlind(
+        -hole_offset - napajeci_kabel / 2)
+    outer_body = outer_body.faces('>Z[-2]').fillet(napajeci_kabel / 2)
+    outer_body = outer_body.faces('<Z').workplane().moveTo(38.5, 0).circle(
+        calculate_pla_bore_diameter(screw_hole) / 2).moveTo(-38.5, 0).circle(
+        calculate_pla_bore_diameter(screw_hole) / 2).cutBlind(-13)
+    outer_body = outer_body.faces('>Z').fillet(12)
+    outer_body = outer_body.faces('<Y').workplane().moveTo(25 + drat_branky / 2, 0).circle(drat_branky / 2).moveTo(
+        -25 - drat_branky / 2, 0).circle(
+        drat_branky / 2).cutThruAll()
+
+    outer_body = outer_body.faces('>Y').workplane().moveTo(-10.5 - 44 / 2 + napajeci_kabel / 2, hole_offset).circle(
+        napajeci_kabel / 2).cutBlind(-width / 2)
+
+    return inner_body.union(outer_body.translate((0, 2 * width, 0)))
+
+
+def main():
+    """Main workflow."""
+    main_case = build_case()
+    render(main_case, 'reolink_zvonek.stl')
+
+
+if __name__ == "__main__":
+    main()
