5fb6356fdd7b1c10f291ba3548ba9caf2481f26c
Petr Vraník <Petr.Vranik@mgm-tp.com>
2026-01-17 15:03:20 +0100

    refactor reolink case model to use dynamic hole positioning

--- smarthome/reolink_zvonek.py
+++ smarthome/reolink_zvonek.py
@@ -17,7 +17,7 @@ def build_case():
     wp = cq.Workplane("XY")
     inner_body = wp.slot2D(height, width).extrude(7)
     inner_body = inner_body.moveTo(10.5, 0).slot2D(44, 20)
-    inner_body = inner_body.moveTo(38.5, 0).circle(screw_hole / 2).moveTo(-38.5, 0).circle(screw_hole / 2).cutThruAll()
+    inner_body = inner_body.moveTo(between_holes/2, 0).circle(screw_hole / 2).moveTo(-between_holes/2, 0).circle(screw_hole / 2).cutThruAll()
     inner_body = inner_body.faces('<Y').workplane().moveTo(ctverec / 2, 0).circle(drat_branky / 2).moveTo(
         -ctverec / 2, 0).circle(
         drat_branky / 2).cutThruAll()
@@ -27,8 +27,8 @@ def build_case():
     outer_body = outer_body.faces('<Z').workplane().moveTo(10.5, 0).slot2D(44, 20).cutBlind(
         -hole_offset - napajeci_kabel / 2)
     outer_body = outer_body.faces('>Z[-2]').fillet(napajeci_kabel / 2)
-    outer_body = outer_body.faces('<Z').workplane().moveTo(38.5, 0).circle(
-        calculate_pla_bore_diameter(screw_hole) / 2).moveTo(-38.5, 0).circle(
+    outer_body = outer_body.faces('<Z').workplane().moveTo(between_holes/2, 0).circle(
+        calculate_pla_bore_diameter(screw_hole) / 2).moveTo(-between_holes/2, 0).circle(
         calculate_pla_bore_diameter(screw_hole) / 2).cutBlind(-13)
     outer_body = outer_body.faces('>Z').fillet(12)
     outer_body = outer_body.faces('<Y').workplane().moveTo(ctverec / 2, 0).circle(drat_branky / 2).moveTo(
