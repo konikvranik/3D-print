f73cef701c7ac74b318a5187b6a73645c21551b2
Petr Vraník <Petr.Vranik@mgm-tp.com>
2026-02-05 16:44:56 +0100

    geometrie

--- smarthome/krabicka_trafa_zvonku.py
+++ smarthome/krabicka_trafa_zvonku.py
@@ -17,8 +17,117 @@ bottom_height = 40
 depth = 150
 WALL_THICKNESS = 3
 
+small_case_width = 40
+small_case_holes_distance = 80
+small_case_height = 10
 
-def build_tapered_prism():
+screw_hole_diameter = 3.2  # For M3 screws
+screw_hole_head_diameter = 5.5  # For M3 screws
+
+
+def build_small_case():
+    """Create a small box with holes for mounting.
+
+    The box is a rectangular prism with specified width and depth,
+    and has mounting holes on the sides.
+    """
+    wp = cq.Workplane("XY")
+    solid = wp.box(
+        small_case_width,
+        small_case_holes_distance + 40,
+        small_case_height,
+        centered=(True, True, False),
+    )
+
+    wp = cq.Workplane("XY")
+    wp = wp.polyline(
+        [
+            (small_case_width / 2, -small_case_holes_distance / 2 - 20),
+            (
+                small_case_width / 2,
+                small_case_holes_distance / 2
+                + screw_hole_head_diameter / 2
+                + WALL_THICKNESS,
+            ),
+            (-small_case_width / 2, -small_case_holes_distance / 2 - 20),
+            (
+                small_case_width / 2 - 5,
+                small_case_holes_distance / 2
+                + screw_hole_head_diameter / 2
+                + WALL_THICKNESS,
+            ),
+        ]
+    ).close()
+    wp = (
+        wp.workplane(offset=small_case_height)
+        .polyline(
+            [
+                (small_case_width / 2, -small_case_holes_distance / 2 - 20),
+                (
+                    small_case_width / 2,
+                    small_case_holes_distance / 2
+                    + screw_hole_head_diameter / 2
+                    + WALL_THICKNESS,
+                ),
+                (-small_case_width / 2, -small_case_holes_distance / 2 - 20),
+                (
+                    small_case_width / 2 - 5,
+                    small_case_holes_distance / 2
+                    + screw_hole_head_diameter / 2
+                    + WALL_THICKNESS,
+                ),
+            ]
+        )
+        .close()
+    )
+
+    # Use Solid.makeLoft() for non-planar loft
+    solid = wp.loft()
+
+    # solid = (
+    #     solid.edges().filter(lambda e: e.Center().z > 0).fillet(small_case_height - 0.1)
+    # )
+    solid = solid.faces("<Z").shell(WALL_THICKNESS)
+
+    # Create mounting holes
+    hole_radius = screw_hole_diameter / 2  # For M3 screws
+    hole_offset_y = small_case_holes_distance / 2
+
+    for y in [-hole_offset_y, hole_offset_y]:
+        x_offset = small_case_width / 2 - screw_hole_head_diameter / 2
+
+        screw_slot = (
+            cq.Workplane("XY")
+            .cylinder(
+                small_case_height,
+                screw_hole_head_diameter / 2 + WALL_THICKNESS,
+                centered=(True, True, False),
+            )
+            .translate((x_offset, y, 0))
+        )
+
+        repeat_to_cut = 5
+        for i in range(repeat_to_cut):
+            screw_slot = (
+                screw_slot.cut(solid).translate((-WALL_THICKNESS, 0, 0)).cut(solid)
+            )
+        screw_slot = screw_slot.translate(((repeat_to_cut) * WALL_THICKNESS, 0, 0))
+
+        solid = solid.union(screw_slot)
+        solid = (
+            solid.moveTo(x_offset, y)
+            .circle(hole_radius)
+            .cutThruAll()
+            .workplane(offset=-WALL_THICKNESS * 2)
+            .moveTo(x_offset, y)
+            .circle(screw_hole_head_diameter / 2)
+            .cutBlind(small_case_height)
+        )
+
+    return solid
+
+
+def build_main_case():
     """Create a prism that starts as 10x10 and tapers to 10x2 over a height.
 
     Uses a loft between two centered rectangles.
@@ -63,10 +172,11 @@ def build_tapered_prism():
 
 
 def cylinder_hole(solid, direction):
+    cylinder_radius = screw_hole_head_diameter / 2 + WALL_THICKNESS
     screw_slot = (
         cq.Workplane("XY")
-        .cylinder(bottom_height / 2, 5, centered=(True, True, False))
-        .translate((direction * (top_width / 2 - 5), 0, 0))
+        .cylinder(bottom_height / 2, cylinder_radius, centered=(True, True, False))
+        .translate((direction * (top_width / 2 - cylinder_radius), 0, 0))
     )
 
     repeat_to_cut = 5
@@ -84,16 +194,17 @@ def cylinder_hole(solid, direction):
         solid.union(screw_slot)
         .faces("<Z")
         .workplane()
-        .moveTo(direction * (top_width / 2 - 5 + WALL_THICKNESS), 0)
-        .circle(1.6)
+        .moveTo(direction * (top_width / 2 - cylinder_radius + WALL_THICKNESS), 0)
+        .circle(screw_hole_diameter / 2)
         .cutThruAll()
         .faces("<Z")
         .workplane(offset=-WALL_THICKNESS * 2)
-        .moveTo(direction * (top_width / 2 - 5 + WALL_THICKNESS), 0)
-        .circle(3)
+        .moveTo(direction * (top_width / 2 - cylinder_radius + WALL_THICKNESS), 0)
+        .circle(screw_hole_head_diameter / 2)
         .cutBlind(-top_height)
     )
 
 
 if __name__ == "__main__":
-    render(build_tapered_prism())
+    # render(build_main_case())
+    render(build_small_case())
