833b5493e15be1c2cc02cbe0cd0d8dca3eebc9b4
Set Your Name <set.your.name@mgm-tp.com>
2026-05-20 13:15:58 +0200

    Refactor rainbird_hose_T_connector.py: improve `build_body` and `koncovka` functions for better readability and flexibility

--- irrigation/rainbird_hose_T_connector.py
+++ irrigation/rainbird_hose_T_connector.py
@@ -42,36 +42,39 @@ M_COLLAR_RADIUS = 8.25
 
 def build_body():
     body = (Workplane("XY")
-            .cylinder(M_COLLAR_RADIUS * 2, M_COLLAR_RADIUS, centered=True)
-            .rotate((0, 0, 0), (0, 1, 0), 90)
-            .cylinder(M_COLLAR_RADIUS, M_COLLAR_RADIUS, centered=(True, True, False))
-            .rotate((0, 0, 0), (1, 0, 0), 90)
-            .translate((M_COLLAR_RADIUS, 0, 0)))
-    body = koncovka(body).translate((-M_COLLAR_RADIUS, 0, 0))
-    body = body.union(koncovka().translate((-M_COLLAR_RADIUS, 0, 0)).rotate((0, 0, 0), (0, 0, 1), 90))
-    body = body.union(koncovka().translate((-M_COLLAR_RADIUS, 0, 0)).rotate((0, 0, 0), (0, 0, 1), 180))
+            .cylinder(M_COLLAR_RADIUS * 2, M_COLLAR_RADIUS, centered=True, direct=(1, 0, 0))
+            .cylinder(M_COLLAR_RADIUS, M_COLLAR_RADIUS, centered=(True, True, False), direct=(0, 1, 0))
+            .faces(">Y").workplane(centerOption="CenterOfBoundBox")
+            .circle(J_INNER_RADIUS).cutBlind(-M_COLLAR_RADIUS)
+            .faces(">X").workplane(centerOption="CenterOfBoundBox")
+            .circle(J_INNER_RADIUS).cutBlind(-2 * M_COLLAR_RADIUS)
+            )
+    body = koncovka(M_COLLAR_RADIUS).union(body)
+    body = koncovka(M_COLLAR_RADIUS).rotate((0, 0, 0), (0, 0, 1), -90).union(body)
+    body = koncovka(M_COLLAR_RADIUS).rotate((0, 0, 0), (0, 0, 1), 180).union(body)
     return body
 
 
-def koncovka(workplane=cq.Workplane("XY")) -> Workplane:
+def koncovka(offset=0.0) -> Workplane:
     """
     Vytvoří model koncovky RainBird konektoru.
     """
 
-    return (workplane.moveTo(-(A + B + C + D + E + F), J_INNER_RADIUS)
-            .line(0, H_STARTING_OUTER_RADIUS - J_INNER_RADIUS)
-            .line(A, 0)
-            .line(B, K_BIG_TOOTH_RADIUS - H_STARTING_OUTER_RADIUS)
-            .line(0, I_OUTER_RADIUS - K_BIG_TOOTH_RADIUS)
-            .line(C, 0)
-            .line(D, L_SMALL_TOOTH_RADIUS - I_OUTER_RADIUS)
-            .line(0, I_OUTER_RADIUS - L_SMALL_TOOTH_RADIUS)
-            .line(E, 0)
-            .line(0, M_COLLAR_RADIUS - I_OUTER_RADIUS)
-            .line(F, 0)
-            .line(0, J_INNER_RADIUS - M_COLLAR_RADIUS)
-            .close()
-            .revolve(angleDegrees=360, axisStart=(0, 0, 0), axisEnd=(1, 0, 0)))
+    return ((cq.Workplane("XY").moveTo(-(A + B + C + D + E + F + offset), J_INNER_RADIUS)
+             .line(0, H_STARTING_OUTER_RADIUS - J_INNER_RADIUS)
+             .line(A, 0)
+             .line(B, K_BIG_TOOTH_RADIUS - H_STARTING_OUTER_RADIUS)
+             .line(0, I_OUTER_RADIUS - K_BIG_TOOTH_RADIUS)
+             .line(C, 0)
+             .line(D, L_SMALL_TOOTH_RADIUS - I_OUTER_RADIUS)
+             .line(0, I_OUTER_RADIUS - L_SMALL_TOOTH_RADIUS)
+             .line(E, 0)
+             .line(0, M_COLLAR_RADIUS - I_OUTER_RADIUS)
+             .line(F, 0)
+             .line(0, J_INNER_RADIUS - M_COLLAR_RADIUS)
+             .close()
+             .revolve(angleDegrees=360, axisStart=(0, 0, 0), axisEnd=(1, 0, 0)))
+    )
 
 
 def main():
