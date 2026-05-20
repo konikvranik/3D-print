342ebadf7e1aa4b73b2acb0ea3ea66d12f830ada
Set Your Name <set.your.name@mgm-tp.com>
2026-05-20 13:52:35 +0200

    Refactor limec.py: improve `build_body` readability, add `THICK` and `SAGITA` constants, and replace `radiusArc` with `sagittaArc`

--- sam/limec.py
+++ sam/limec.py
@@ -6,14 +6,22 @@ import cadquery as cq
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from common import render
 
-WIDTH=230
+WIDTH = 230
+THICK = 20
+SAGITA = 70
 
 
 def build_body():
-    """Vytvoří model stolního držáku na křeslo.
+    """Vytvoří model límce z CyberPunku.
     """
 
-    body=cq.Workplane("XY").moveTo(0,0).radiusArc((WIDTH, 0), 100).line(0, -50).radiusArc((0, -50),-100) .lineTo(0, 0).close().extrude(20)
+    body = (cq.Workplane("XY")
+            .moveTo(0, 0)
+            .sagittaArc((WIDTH, 0), SAGITA)
+            .line(0, -THICK)
+            .sagittaArc((0, -THICK), -SAGITA)
+            .close()
+            .extrude(20))
     return body
 
 
