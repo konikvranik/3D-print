ef79a30616211699221a2c21693eab7ab8f72b06
Petr Vraník <petr.vranik@mgm-tp.com>
2026-05-31 17:16:20 +0200

    Refactor kolébkový_reverzační_mechanismus.py to use cq_gears for gear generation and update pyproject.toml to switch to hatchling build backend

--- smarthome/kolébkový reverzační mechanismus/kolébkový reverzační mechanismus.py
+++ smarthome/kolébkový reverzační mechanismus/kolébkový reverzační mechanismus.py
@@ -1,82 +1,45 @@
 import cadquery as cq
+import cq_gears as gear
 import math
 
 # --- PARAMETRY ---
-modul = 1.5
-vule = 0.2  # Vůle pro volný rotační pohyb dílů (0.2mm je ideální pro 3D tisk)
+modul = 2
+vule = 0.2  # Vůle pro volný rotační pohyb (0.2mm pro 3D tisk)
+tloustka_kol = 8.0
+hridel_motoru = 8.0 / 2  # Poloměr hřídele (4mm)
 
 zuby_slunce = 12
-zuby_satelit = 10
+zuby_satelit = 12
 
-d_slunce = modul * zuby_slunce
-d_satelit = modul * zuby_satelit
+# --- 1. GENEROVÁNÍ OZUBENÝCH KOL POMOCÍ CQ_GEARS ---
 
-tloustka_kol = 8.0
-hridel_motoru = 8.0 / 2
-osova_vzdalenost_ramene = (d_slunce + d_satelit) / 2
+# Převod cq_gears tvaru na standardní CadQuery Workplane
+slunce = cq.Workplane("XY").add(
+    gear.SpurGear(module=modul, teeth_number=zuby_slunce, width=tloustka_kol, bore_d=8.0).build())
 
-uhel_v = 40.0
+# Vyříznutí plošky pro D-profil hřídele (hloubka 0.5mm z průměru)
+slunce = slunce.faces(">Z").workplane().rect(8.0, 0.5).extrude(-tloustka_kol, combine="cut")
+
+satelit = cq.Workplane("XY").add(
+    gear.SpurGear(module=modul, teeth_number=zuby_satelit, width=tloustka_kol, bore_d=5.0 + vule).build())
+
+# --- 2. VÝPOČET GEOMETRIE RAMENE ---
+# Matematicky přesný výpočet roztečného poloměru (r = (m * z) / 2)
+r_slunce = (modul * zuby_slunce) / 2.0
+r_satelit = (modul * zuby_satelit) / 2.0
+
+osova_vzdalenost_ramene = r_slunce + r_satelit
+
+uhel_v = 52.5
 uhel_rad = math.radians(uhel_v)
 
-# Výpočet pozic konců ramen
+# Pozice středů satelitů
 x_l = osova_vzdalenost_ramene * math.sin(-uhel_rad)
 y_l = osova_vzdalenost_ramene * math.cos(-uhel_rad)
 x_p = osova_vzdalenost_ramene * math.sin(uhel_rad)
 y_p = osova_vzdalenost_ramene * math.cos(uhel_rad)
 
-# --- FUNKCE PRO OZUBENÉ KOLO ---
-def vytvor_ozubene_kolo(zuby, modul, tloustka, prumer_diry):
-    r_roztecna = (modul * zuby) / 2
-    r_hlavova = r_roztecna + modul
-    r_patova = r_roztecna - 1.25 * modul
-    r_valce = r_patova + 0.1
-
-    kolo = cq.Workplane("XY").cylinder(tloustka, r_valce)
-
-    uhel_zubu = 360.0 / zuby
-    sirka_hlavy = modul * 0.5
-    sirka_paty = modul * 1.25
-
-    for i in range(zuby):
-        uhel_deg = i * uhel_zubu
-        uhel_rad = math.radians(uhel_deg)
-
-        dir_x = math.sin(uhel_rad)
-        dir_y = math.cos(uhel_rad)
-        tan_x = math.cos(uhel_rad)
-        tan_y = -math.sin(uhel_rad)
-
-        p1 = ((r_patova - 0.2) * dir_x - (sirka_paty/2) * tan_x, (r_patova - 0.2) * dir_y - (sirka_paty/2) * tan_y)
-        p2 = (r_hlavova * dir_x - (sirka_hlavy/2) * tan_x, r_hlavova * dir_y - (sirka_hlavy/2) * tan_y)
-        p3 = (r_hlavova * dir_x + (sirka_hlavy/2) * tan_x, r_hlavova * dir_y + (sirka_hlavy/2) * tan_y)
-        p4 = ((r_patova - 0.2) * dir_x + (sirka_paty/2) * tan_x, (r_patova - 0.2) * dir_y + (sirka_paty/2) * tan_y)
-
-        zub = (
-            cq.Workplane("XY")
-            .workplane(offset=-tloustka/2)
-            .moveTo(p1[0], p1[1])
-            .lineTo(p2[0], p2[1])
-            .lineTo(p3[0], p3[1])
-            .lineTo(p4[0], p4[1])
-            .close()
-            .extrude(tloustka)
-        )
-        kolo = kolo.union(zub)
-
-    if prumer_diry > 0:
-        kolo = kolo.faces(">Z").workplane().hole(prumer_diry * 2)
-
-    return kolo
-
-# --- 1. CENTRÁLNÍ KOLO (SLUNCE) ---
-slunce = vytvor_ozubene_kolo(zuby_slunce, modul, tloustka_kol, hridel_motoru)
-slunce = slunce.faces(">Z").workplane().rect(8.0, 0.5).extrude(-tloustka_kol, combine="cut")
-
-# --- 2. SATELITNÍ KOLO ---
-satelit = vytvor_ozubene_kolo(zuby_satelit, modul, tloustka_kol, (5.0 + vule) / 2)
-
-# --- 3. VÝKYVNÉ RAMENO S ČEPY A DRÁŽKAMI ---
-# Základní deska ramene
+# --- 3. VÝKYVNÉ RAMENO S ČEPY A DRÁŽKAMI PRO SÉGROVKY ---
 rameno_deska = (
     cq.Workplane("XY")
     .cylinder(4.0, hridel_motoru + 4.0)
@@ -87,7 +50,7 @@ rameno_deska = (
     .faces(">Z").workplane().hole((hridel_motoru * 2) + vule)
 )
 
-# Tvorba čepů (os)
+# Tvorba čepů (os pro satelity, průměr 5mm, výška 11mm)
 cepy = (
     cq.Workplane("XY")
     .workplane(offset=2.0)
@@ -95,10 +58,9 @@ cepy = (
     .moveTo(x_p, y_p).circle(2.5).extrude(11.0)
 )
 
-# Sloučení základny a čepů do jednoho solidu
 rameno_komplet = rameno_deska.union(cepy)
 
-# Tělesa pro vyříznutí drážek (odčítaný solid)
+# Solidy pro vyříznutí drážek na ségrovky
 drazka_leva = (
     cq.Workplane("XY")
     .workplane(offset=2.0 + 8.5)
@@ -114,7 +76,6 @@ drazka_prava = (
     .extrude(1.2)
 )
 
-# Finální vyříznutí drážek z ramene
 rameno = rameno_komplet.cut(drazka_leva).cut(drazka_prava)
 
 # --- 4. POJISTNÝ C-KLIP (SÉGROVKA) ---
@@ -125,7 +86,7 @@ klip = (
     .faces(">Z").workplane().rect(10.0, 3.2).extrude(-1.0, combine="cut")
 )
 
-# --- SESTAVENÍ DO ASSEMBLY ---
+# --- SESTAVENÍ DO ASSEMBLY A MANUÁLNÍ POLOHOVÁNÍ ---
 sestava = cq.Assembly()
 
 sestava.add(slunce, name="stredove_slunce", color=cq.Color("red"))
@@ -140,5 +101,6 @@ sestava.add(klip, name="pojistka_leva", color=cq.Color("blue"),
 sestava.add(klip, name="pojistka_prava", color=cq.Color("blue"),
             loc=cq.Location(cq.Vector(x_p, y_p, tloustka_kol/2 + 1.0)))
 
-sestava.save("kolebkovy_mechanismus_sestava.step", "STEP")
-print("Hierarchická sestava exportována bez chyb.")
+# Export do jednoho STEP souboru
+sestava.save("kolebkovy_mechanismus_cq_gears.step", "STEP")
+print("Sestava úspěšně uložena.")
